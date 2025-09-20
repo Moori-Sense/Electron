1770c159c8025b02345ce07b2e5ad2279864176a
junhui <junhuilim@kakao.com>
2025-09-21 00:25:34 +0900

    fix: 저장 수정

--- electron/main.ts
+++ electron/main.ts
@@ -31,7 +31,7 @@ process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 
 let win: BrowserWindow | null
 
 // 메인 프로세스용 날씨 서비스 import
-import { fetchWeatherDataMain } from '../src/services/weatherServiceMain'
+import { fetchWeatherDataMain } from '../src/services/weatherService'
 
 // IPC 핸들러 등록
 ipcMain.handle('get-weather-data', async () => {
