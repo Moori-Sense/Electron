b4e2c5a2d427a843ef13a3fd51042e86a4c17a55
LimJH01 <lim.jh.gud@gmail.com>
2025-09-20 17:22:32 +0900

    loading/main screen

---
+++ src/components/MainScreen.tsx
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function MainScreen() {
+  return (
+    <div>
+      <h1>MooriSense Dashboard</h1>
+      <p>System is running.</p>
+      {/* 여기에 앞으로 만들 실제 대시보드 UI가 들어갑니다. */}
+    </div>
+  );
+}
+
+export default MainScreen;
